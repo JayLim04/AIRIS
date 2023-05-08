66c2834fd450f921f8024d256fafe3d537d842f9
Kittymittens11 <21033869@myrp.edu.sg>
2023-05-08 17:03:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21033869/airis/mainPage.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21033869.airis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
