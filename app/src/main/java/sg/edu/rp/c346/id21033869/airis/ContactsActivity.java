0db54638f1de4aacdf9cd74ecfe633491c54ca40
NTL RP <21023644@myrp.edu.sg>
2023-08-06 15:25:49 +0800

    alan implementation

--- app/src/main/java/sg/edu/rp/c346/id21033869/airis/ContactsActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21033869/airis/ContactsActivity.java
@@ -10,31 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactsActivity extends AppCompatActivity {
 
-    Button btnJohn, btnJim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        btnJohn = findViewById(R.id.buttonCallJohn);
-        btnJim = findViewById(R.id.buttonCallJim);
-
-        btnJohn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" +92240336));
-                startActivity(intentCall);
-            }
-        });
-
-        btnJim.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" +89675543));
-                startActivity(intentCall);
-            }
-        });
     }
 
 }
